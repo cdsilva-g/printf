75bc70e235d66d832208ea4e8234fe90286cbb4f
NyangoyaG <nyangoyagb@gmail.com>
2023-01-28 22:53:44 -0800

    Updated main file

--- main.h
+++ main.h
@@ -1,10 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-int _putchar(char c);
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
 int _printf(const char *format, ...);
-int printf_char(va_list list);
-int printf_str(va_list list);
-#endif /*end of MAIN_H */
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
+#endif
